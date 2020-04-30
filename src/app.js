c4614901e24989d9cbfcffe4d63257b7b4b36b70
SACHIN KALONIA <sachin@farmart.co>
2020-04-30 11:52:12 +0530

    addded useLink

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 "use strict";
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useLinking } from "@react-navigation/native";
 /* Redux */
 import { Provider } from "react-redux";
 import store from "./redux/store";
@@ -11,9 +11,45 @@ import AppContainer from "./navigators";
 /* Component */
 
 const App = () => {
+
+    const ref = React.useRef();
+
+  const { getInitialState } = useLinking(ref, {
+    prefixes: ['https://mychat.com', 'mychat://'],
+  });
+
+  const [isReady, setIsReady] = React.useState(false);
+  const [initialState, setInitialState] = React.useState();
+
+    React.useEffect(() => {
+        Promise.race([
+            getInitialState(),
+            new Promise(resolve =>
+            // Timeout in 150ms if `getInitialState` doesn't resolve
+            // Workaround for https://github.com/facebook/react-native/issues/25675
+                setTimeout(resolve, 150)
+            ),
+        ])
+        .catch(e => {
+            console.error(e);
+        })
+        .then(state => {
+            if (state !== undefined) {
+            setInitialState(state);
+            }
+
+            setIsReady(true);
+        });
+    }, [getInitialState]);
+
+    if (!isReady) {
+        return null;
+    }
+
+
     return (
         <Provider store={store}>
-            <NavigationContainer>
+            <NavigationContainer initialState={initialState} ref={ref} >
                 <AppContainer/>
             </NavigationContainer>
         </Provider>
